eb68fd6615f210ce17d6e0c03b1e427b0f8ec262
Adam <adamrobertbolton@gmail.com>
2021-03-11 18:47:35 +0000

    Initial download from old

---
+++ cdn.lerndmina.dev/assets/php/head.php
@@ -0,0 +1,15 @@
+<!-- Tags for socials -->
+<meta name="og:title" content="">
+<meta name="description" content="">
+<meta name="og:description" content="">
+<meta name="keywords" content="">
+<meta name="theme-color" content="#c671ff">
+
+<link rel="icon" href="/assets/img/favicon.ico">
+<link href="/assets/css/bootstrap.css" rel="stylesheet">
+<link href="/assets/css/style.css" rel="stylesheet">
+
+
+<!-- FONT -->
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@500&display=swap" rel="stylesheet"> 
