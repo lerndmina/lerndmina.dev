bc42ce59cbd68795613d6b62961a3bf7d6da90f2
Adam <adamrobertbolton@gmail.com>
2021-03-15 23:26:52 +0000

    Update index.php

--- lerndmina.dev/index.php
+++ lerndmina.dev/index.php
@@ -16,7 +16,7 @@
          </type></h3>
      <h3><!--List stuff here--></h3>
      <p>
-         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Iaculis urna id volutpat lacus laoreet non. Eget est lorem ipsum dolor sit amet consectetur adipiscing elit. Pellentesque eu tincidunt tortor aliquam nulla. Pellentesque eu tincidunt tortor aliquam nulla. Quis viverra nibh cras pulvinar mattis nunc sed blandit. Fermentum posuere urna nec tincidunt praesent. Mauris cursus mattis molestie a iaculis. Auctor neque vitae tempus quam pellentesque nec nam aliquam sem. Vitae sapien pellentesque habitant morbi tristique senectus et netus. Nibh mauris cursus mattis molestie a iaculis at erat pellentesque. Consectetur adipiscing elit ut aliquam. Aliquam sem fringilla ut morbi tincidunt augue.
+         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Iaculis urna id volutpat lacus laoreet non. Eget est lorem ipsum dolor sit amet consectetur adipiscing elit. Pellentesque eu tincidunt tortor aliquam nulla. Pellentesque eu tincidunt tortor aliquam nulla. Quis viverra nibh cras pulvinar mattis nunc sed blandit. Fermentum posuere urna nec tincidunt praesent. Mauris cursus mattis molestie a iaculis. Auctor neque vitae tempus quam pellentesque nec nam aliquam sem. Vitae sapien pellentesque habitant morbi tristique senectus et netus. Nibh mauris cursus mattis molestie a iaculis at erat pellentesque. Consectetur adipiscing elit ut aliquam. Aliquam sem fringilla ut morbi tincidunt augue..
      </p>
      <img src="" alt="A very Cool Image Here" class="img">
 </div>  <!--  Closes Content Wrap  -->
