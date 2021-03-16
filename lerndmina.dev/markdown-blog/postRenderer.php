315c35f2b00738f721f6f1ea54103ffb3264f965
Adam <adamrobertbolton@gmail.com>
2021-03-16 16:13:31 +0000

    updated gitignore.

--- lerndmina.dev/markdown-blog/postRenderer.php
+++ lerndmina.dev/markdown-blog/postRenderer.php
@@ -1,5 +1,6 @@
 <?php
     require_once __DIR__ .'/../lib/Parsedown.php';
+    include_path='C:\xampp\php\PEAR';
     require_once __DIR__ .'/../lib/ParsedownExtra.php';
     require_once __DIR__ .'/settings.php';
 
