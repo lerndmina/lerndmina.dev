6dc8c8a74abf669f8dd1560c9a5b5eb94fc69ab4
Adam <adamrobertbolton@gmail.com>
2021-03-21 17:45:59 +0000

    Added logo to portfolio, added icons for contact.

--- lerndmina.dev/index.php
+++ lerndmina.dev/index.php
@@ -9,16 +9,20 @@
  <div id="content-wrap">
      <?php require_once($_SERVER['DOCUMENT_ROOT'].'/assets/php/nav.php') ?>
      <h1>Welcome </h1>
-     <h2>My name is Adam.</h2>
+     <h2>I'm Wild.</h2>
      <h2>I'm skilled in 
-         <type class="typewrite gradient" data-period="3000" data-type='[ "Web Development.", "Systems Administration.", "Server Management.", "Customer Support." ]'>
-             <noscript>Web Design, SysAdmin, Server Management.</noscript>
+         <type class="typewrite gradient" data-period="3000" data-type='[ "Web Development.", "Systems Administration.", "Customer Support." ]'>
+             <noscript>Web Design, SysAdmin.</noscript>
          </type></h2>
-     <h3><!--List stuff here--></h3>
-     <p>
-         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Iaculis urna id volutpat lacus laoreet non. Eget est lorem ipsum dolor sit amet consectetur adipiscing elit. Pellentesque eu tincidunt tortor aliquam nulla. Pellentesque eu tincidunt tortor aliquam nulla. Quis viverra nibh cras pulvinar mattis nunc sed blandit. Fermentum posuere urna nec tincidunt praesent. Mauris cursus mattis molestie a iaculis. Auctor neque vitae tempus quam pellentesque nec nam aliquam sem. Vitae sapien pellentesque habitant morbi tristique senectus et netus. Nibh mauris cursus mattis molestie a iaculis at erat pellentesque. Consectetur adipiscing elit ut aliquam. Aliquam sem fringilla ut morbi tincidunt augue..
-     </p>
-     <img src="" alt="A very Cool Image Here" class="img">
+     <p>Contact Me:</p>
+     <div class="socials">
+             <a href="https://dsc.bio/wild" target="_blank">
+                 <img src="/assets/img/svg/discord.svg" alt="A very Cool Image Here" class="svg img">
+             </a>
+             <a href="mailto:wild@lerndmina.dev" target="_blank">
+                 <img src="/assets/img/svg/email.svg" alt="A very Cool Image Here" class="svg img svg-makewhite">
+             </a>
+     </div>
 </div>  <!--  Closes Content Wrap  -->
 
         <!--    Footer Here-->
--- lerndmina.dev/portfolio/themes/default/header.php
+++ lerndmina.dev/portfolio/themes/default/header.php
@@ -19,17 +19,47 @@
 <meta property="twitter:image" content="/assets/img/logos/logo-name-bottom-min-transparent.png">
 </head>
 <body>
+<a class="logo-custom" href="/">
+        <!--    <img src="/assets/img/logos/3.jpg" class="logo">-->
+        &lt;/Lerndmina.Dev&gt
+</a>
 	<header class="site-header nav-size">
-		<a href="/">
+        <style>
+            .logo-custom{
+                background: hsla(324, 91%, 46%, 1);
+                background: linear-gradient(0deg, hsl(324, 91%, 46%) 0%, hsl(18, 100%, 49%) 100%);
+                background: -moz-linear-gradient(0deg, hsla(324, 91%, 46%, 1) 0%, hsla(18, 100%, 49%, 1) 100%);
+                background: -webkit-linear-gradient(0deg, hsla(324, 91%, 46%, 1) 0%, hsla(18, 100%, 49%, 1) 100%);
+                -webkit-background-clip: text;
+                -webkit-text-fill-color: transparent;
+                padding-top: 0.2em; /* Fix on mobile where the text gets cut off */
+                margin-top: -0.2em; /* Very temporary solotion*/
+                display: block;
+                margin-left: 5px;
+                margin-right: auto;
+                max-width: 300px;
+                height: auto;
+                font-family: 'Roboto', sans-serif;
+                font-weight: 400;
+                font-size: 1.15rem;
+                text-decoration: none;
+                /*position: absolute;*/
+            }
+            .no-underline{
+                text-decoration: none;
+            }
+        </style>
+
+        <a class="no-underline" href="/">
 		<!-- <div class="logo">&lt;/l&gt</div> -->
 		</a>
-		<a href="/about"> 
+		<!--<a href="/about">
 			About Me
-		</a> - 
-		<a href="/">
+		</a> - -->
+		<a class="no-underline" href="/">
 			 Home
 		</a> - 
-		<a href="<?= $config['base_url'] ?>/">
+		<a class="no-underline" href="<?= $config['base_url'] ?>/">
 			Portfolio
 		</a>
 	</header>
