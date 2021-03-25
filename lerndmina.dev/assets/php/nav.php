25eca949f2337d817b3d9f175b71ebeb3bce09fc
Adam <adamrobertbolton@gmail.com>
2021-03-25 20:11:29 +0000

    Nebula's testsite uploaded.

--- lerndmina.dev/assets/php/nav.php
+++ lerndmina.dev/assets/php/nav.php
@@ -1,6 +1,8 @@
 <div class="logo"><a class="gradient" href="/">
 <!--    <img src="/assets/img/logos/3.jpg" class="logo">-->
-        &lt;/Lerndmina.Dev&gt
+<!--        &lt;/Lerndmina.Dev&gt-->
+        &lt;Nebu//a&gt
+
 </div></a>
 
 <div class="nav-size nav-all">
---
+++ test.lerndmina.dev/php/nav.php
@@ -0,0 +1,10 @@
+<img src="#" alt="A very cewl logo goes here, probably.">
+
+<div class="nav">
+    <a href="#">Navlink</a>
+    <a href="#">Navlink</a>
+    <a href="#">Navlink</a>
+    <a href="#">Navlink</a>
+    <a href="#">Navlink</a>
+    <a href="#">Navlink</a>
+</div>
