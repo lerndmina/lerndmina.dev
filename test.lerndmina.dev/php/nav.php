098bf03034fb5096b64319d3b0b50d9f6413d140
Adam <adamrobertbolton@gmail.com>
2021-03-25 20:36:11 +0000

    Added Nebula Logo

--- test.lerndmina.dev/php/nav.php
+++ test.lerndmina.dev/php/nav.php
@@ -1,5 +1,6 @@
-<img src="#" alt="A very cewl logo goes here, probably.">
-
+<a href="/">
+    <img class="logo" src="/img/Nebula_Code_NoBG.png" alt="A very cewl logo goes here, probably.">
+</a>
 <div class="nav">
     <a href="#">Navlink</a>
     <a href="#">Navlink</a>
