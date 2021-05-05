b695be4dc7f02311f58b9207adfaee212d88a22b
Adam <adamrobertbolton@gmail.com>
2021-05-05 23:13:13 +0100

    Update nav.php

--- lerndmina.dev/assets/php/nav.php
+++ lerndmina.dev/assets/php/nav.php
@@ -1,7 +1,7 @@
 <div class="logo"><a class="gradient" href="/">
 <!--    <img src="/assets/img/logos/3.jpg" class="logo">-->
-<!--        &lt;/Lerndmina.Dev&gt-->
-        &lt;Nebu//a&gt
+        &lt;/Lerndmina.Dev&gt
+        <!-- &lt;Nebu//a&gt -->
 
 </div></a>
 
