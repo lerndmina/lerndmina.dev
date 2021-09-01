7f133facca84a75d6918e2221385a18806cff8c6
Wild <wild@lerndmina.dev>
2021-09-01 18:36:41 +0100

    Changes

--- assets/php/head.php
+++ assets/php/head.php
@@ -34,8 +34,9 @@ This is just my little portfolio. Come check me out!">
 
 
 <!--FONTS-->
-<link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Exo:wght@300&family=Questrial&family=Sanchez&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
 <!--/FONTS-->
 
